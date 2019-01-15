a9654b104b27cec6eb7211f9e9a6eec9846cc69a
Siddhant Viksit <siddhantrai2003@gmail.com>
2019-01-15 19:45:33 +0530

    Added ability to view comments

--- til.py
+++ til.py
@@ -42,20 +42,27 @@ else:
     for i,submission in enumerate(list_sub):
         print(f'{i+1}. {submission.title[3:]}\n')
 
+num_of_link = 1
 while True:
     a = input('>')
-    num_of_link = 0
     if a == 'help':
         with open('help','r') as f:
             print(f.read())
     elif a == 'quit':
         break
+    elif a == 'view':
+        subprocess.run(['w3m',list_sub[num_of_link].url])
+    elif a == 'comments':
+        for i,comment in enumerate(list_sub[num_of_link].comments):
+            print(comment.body)
+            print('#'*100)
+            if i == 5:
+                break
+
     else:
         try:
             num_of_link = int(a) - 1
         except ValueError:
             print('Enter "help" for help')
-        else:
-            subprocess.run(['w3m',list_sub[num_of_link].url])
 
 sys.exit(0)
